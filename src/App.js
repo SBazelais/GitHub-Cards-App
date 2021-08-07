07d7ef7960d151661af920c8cf8f555fa78de1f9
Steve Bazelais <57912340+SBazelais@users.noreply.github.com>
2021-08-06 22:49:02 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import React, { Component } from "react";
+import "./styles.scss";
+
+const CardList = (props) => (
+  <div>
+    {props.profiles.map((profile) => (
+      <Card key={profile.id} {...profile} />
+    ))}
+  </div>
+);
+
+class Card extends Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url} alt="user" />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Form extends Component {
+  state = {
+    userName: ""
+  };
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const resp = await axios.get(
+      `https://api.github.com/users/${this.state.userName}`
+    );
+    this.props.onSubmit(resp.data);
+    this.setState({ userName: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="GitHub username"
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          required
+        />
+        <button> Add card</button>
+      </form>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    profiles: []
+  };
+
+  addNewProfile = (profileData) => {
+    this.setState((prevState) => ({
+      profiles: [...prevState.profiles, profileData]
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
+}
+
+export default App;
